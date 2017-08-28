e3f7073ab2f5df7976da58becfe5b5f792fb06c1
xudaning <xudaning@qq.com>
2017-08-29 00:01:34 +0800

    第一次提交

---
+++ src/GitGood.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/8/28 0028.
+ */
+public class GitGood {
+    public static void main(String[] args) {
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hahah");
+    }
+}
