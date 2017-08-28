f0e4fab3cb91c6b0d23872edfafd7d84a43b8969
xudaning <xudaning@qq.com>
2017-08-29 00:20:12 +0800

    耿显第一次提交

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,6 +1,8 @@
-
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello,git");
+        System.out.println("sdfasdf");
+        System.out.println("sdfasdf");
+        System.out.println("sdfasdf");
     }
 }
---
+++ src/hahaha.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/8/29 0029.
+ */
+public class hahaha {
+    public static void main(String[] args) {
+        System.out.println("new");
+    }
+}
